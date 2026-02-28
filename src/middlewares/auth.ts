ac30bea967fb1e273729a82a44864a7844e27249
Dushyantsingh4 <developerdushynat99@gmail.com>
2026-02-28 08:45:07 +0530

    added auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,13 +1,28 @@
 import { Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 import { AuthRequest } from "../types";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-        res.status(401).json({ message: "Unauthorized: No Token Provided" });
+        return res.status(401).json({ message: "Unauthorized: No Token Provided" });
     }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized: Invalid Token Format" });
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Forbidden: Invalid Token' });
+        }
+
+        req.user = decoded;
+        next();
+    });
 }
